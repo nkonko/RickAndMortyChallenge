1734785c41abea729917475b946d12abc5052957
nkonko <nicoazzara@hotmail.com>
2024-06-09 16:30:34 +0200

    added spinner interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/nav/nav.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavComponent],
+  imports: [CommonModule, RouterOutlet, NavComponent,SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/core/services/spinner.service.ts
+++ src/app/core/services/spinner.service.ts
@@ -5,14 +5,21 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class SpinnerService {
+  private requestCount = 0;
   private spinnerSubject = new BehaviorSubject<boolean>(false);
   spinner$ = this.spinnerSubject.asObservable();
 
   show() {
+    this.requestCount++;
     this.spinnerSubject.next(true);
   }
 
   hide() {
-    this.spinnerSubject.next(false);
+    if (this.requestCount > 0) {
+      this.requestCount--;
+    }
+    if (this.requestCount === 0) {
+      this.spinnerSubject.next(false);
+    }
   }
 }
